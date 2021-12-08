024ee0a9880fab73662dffeb8357944cb32d3c9d
Sarai Misidjan <Sarai@conduction.nl>
2021-12-08 17:10:15 +0100

    fix cases page

--- pwa/src/pages/cases/index.tsx
+++ pwa/src/pages/cases/index.tsx
@@ -1,48 +1,48 @@
 import * as React from "react";
 import Layout from "../../components/common/layout";
-import { useUrlContext } from "../../context/urlContext";
-import { useUserContext } from "../../context/userContext";
-import { Link } from "gatsby";
 import DigiDImage from "../../images/digid_button.svg";
 import { Table } from "@conductionnl/nl-design-system/lib/Table/src/table";
 import {MainActionMenu} from "../../components/common/actionMenu";
+import {getUser, isLoggedIn} from "../../services/auth";
 
 const IndexPage = () => {
-  const context = useUrlContext();
-  const user = useUserContext().user;
-
+  const [context, setContext] = React.useState(null);
   const [dossiers, setDossiers] = React.useState(null);
 
-  const getDossiers = () => {
-    fetch(context.apiUrl + '/gateways/vrijbrp_dossiers/api/v1/dossiers/search', {
-      method: 'POST',
-      credentials: 'include',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({
-        "bsns": [
-          '693789128'
-        ],
-      })
-    })
-      .then(response => response.json())
-      .then((data) => {
-        console.log(data);
-        if (data.result !== undefined && data.result.content !== undefined) {
-          let dossiers = data.result.content;
-          for (let i = 0; i < dossiers.length; i++) {
-            dossiers[i].id = i;
-          }
-          setDossiers(dossiers);
-        } else {
-          setDossiers(null);
-        }
-      });
-  }
-
   React.useEffect(() => {
-    console.log(user);
-    getDossiers();
-  }, []);
+    if (typeof window !== "undefined" && context === null) {
+      setContext({
+        apiUrl: window.GATSBY_API_URL,
+        frontendUrl: window.GATSBY_FRONTEND_URL,
+      });
+    } else {
+      if (isLoggedIn()) {
+        fetch(`${context.apiUrl}/gateways/vrijbrp_dossiers/api/v1/dossiers/search`, {
+          method: 'POST',
+          credentials: 'include',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+            "bsns": [
+              '693789128'
+            ],
+          })
+        })
+          .then(response => response.json())
+          .then((data) => {
+            console.log(data);
+            if (data.result !== undefined && data.result.content !== undefined) {
+              let dossiers = data.result.content;
+              for (let i = 0; i < dossiers.length; i++) {
+                dossiers[i].id = i;
+              }
+              setDossiers(dossiers);
+            } else {
+              setDossiers(null);
+            }
+          });
+      }
+    }
+  }, [context]);
 
   return (
     <Layout>
@@ -58,7 +58,7 @@ const IndexPage = () => {
             Aanvragen
           </h1>
           {
-            dossiers !== null ? 
+            dossiers !== null ?
             <Table columns={[{ headerName: "ID", field: "id" }, { headerName: "Description", field: "description" }, { headerName: "Start date", field: "startDate" }]} rows={dossiers} />
             :
             <p className="utrecht-paragraph">Geen resultaten gevonden</p>
