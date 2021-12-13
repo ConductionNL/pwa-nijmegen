508cf845c5b25aa9aa5233b25affbc92a4ef0fb6
smisidjan <smisidjan@users.noreply.github.com>
2021-12-13 08:44:44 +0000

    deployed via Gatsby Publish Action 🎩 for 6f0715f9be1c7dfa0b76fd211c4acc7f880f9d15

---
+++ env.js
@@ -0,0 +1,8 @@
+window.GATSBY_API_URL = "http://localhost/api";
+window.GATSBY_ME_URL = "http://localhost/me";
+window.GATSBY_BASE_URL = "http://localhost";
+window.GATSBY_FRONTEND_URL = "http://localhost:8000";
+window.GATSBY_ORGANIZATION = "001516814";
+window.GATSBY_REDIRECT_URL = "null";
+window.GATSBY_DEFAULT_THEME = "nijmegen";
+window.WAARDEPAPIEREN_PAYMENT_REQUIRED = false;
