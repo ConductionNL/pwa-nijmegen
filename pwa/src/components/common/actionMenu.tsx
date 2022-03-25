fbf17597272472ec50aca93eb6b53f02a1c5de42
Sarai Misidjan <sarai@conduction.nl>
2022-03-25 10:59:42 +0100

    new action menu

--- pwa/src/components/common/actionMenu.tsx
+++ pwa/src/components/common/actionMenu.tsx
@@ -1,10 +1,11 @@
 import * as React from "react";
-import {ActionMenu, BreakpointActionMenu} from "@conductionnl/nl-design-system/lib/ActionMenu/src/actionMenu";
+import { ActionMenu, BreakpointActionMenu } from "@conductionnl/nl-design-system/lib/ActionMenu/src/actionMenu";
 
 export function MainActionMenu() {
   return (
     <ActionMenu
-      items={[{ name: 'Diensten', icon: 'fas fa-shopping-cart', link: '/products' }, { name: 'Mijn aanvragen', icon: 'fas fa-list-alt', link: '/cases' }, { name: 'Mijn gegevens', icon: 'fas fa-id-card-alt', link: '/data' }, { name: 'Mijn kluis', icon: 'fas fa-lock', link: '/vault' }]}
+      items={[{ name: 'Verwerkingsregister', icon: 'fas fa-book', link: '/register' },
+       { name: 'Contracten inzien', icon: 'fas fa-file-contract', link: '/contract' }]}
       breakpoint={BreakpointActionMenu.mobile}
     />
   );
